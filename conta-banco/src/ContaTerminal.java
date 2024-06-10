6aa9734b6bb92a45ce47cc68b025b7bcb39184fa
wallLuiz <wallaceluiz30.silva@gmail.com>
2024-06-10 15:42:57 -0300

    implementação de acordo com o Desafio proposto, com algumas coisas adicionais.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,46 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero; double saldo; String agencia; String nomeCliente;   
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o seu Nome");
+            nomeCliente = scanner.nextLine();
+        
+            while (true) {
+                System.out.println("Por favor, digite a Agência");
+                agencia = scanner.nextLine();
+                if (agencia.length() <= 5) {
+                    break;
+                } else {
+                    System.out.println("A entrada deve ter no máximo 4 caracteres.");
+                }
+            }
+   
+            while (true) {
+                System.out.println("Por favor, digite o número da Agência");
+                numero = scanner.nextInt();
+                if (numero <= 9999) {
+                    break;
+                } else {
+                    System.out.println("o limite para a aprovação é 4 digitos");
+                }
+            }
+        
+            while (true) {
+                System.out.println("Por favor, digite o saldo");
+                saldo = scanner.nextDouble();
+                if (saldo <= 999.99) {
+                    break;
+                } else {
+                    System.out.println("o limite para a aprovação é 999.99");
+                }
+            }
+        
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta " +numero+ " e seu saldo de crédito " +saldo+ " foi aprovado e já está disponível para saque");
+        
+        scanner.close();
     }
+        
 }
